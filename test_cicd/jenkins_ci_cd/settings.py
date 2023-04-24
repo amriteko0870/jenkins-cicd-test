78b5a10eda1329c1af67af632d334a5c85fe1d7e
AmritSingh <amrit.s@ekoinfomatics.com>
2023-04-24 18:21:54 +0530

    allowed all host

--- test_cicd/jenkins_ci_cd/settings.py
+++ test_cicd/jenkins_ci_cd/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-bftn7$6n)e(ivk+n_)m414(vbqjp3tm0ms02qqje0%%nbh2b%4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
