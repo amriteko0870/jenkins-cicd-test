e07d7b5bde3244c80ce0d60445f9fbf4c56d9ea4
AmritSingh <amrit.s@ekoinfomatics.com>
2023-04-24 17:38:34 +0530

    script for django server update and run

--- test_script.sh
+++ test_script.sh
@@ -7,4 +7,10 @@ else
     python3 -m virtualenv venv
     echo "virtual environment created"
 fi
+
+source venv/bin/activate
+cd test_cicd
+pip install -r requirements.txt
+kill -9 $(lsof -t -i:8000)
+nohup python manage.py runserver 0.0.0.0:8000
 # mkdir test_folder_new
