b3856f8cc7c30a272c71f6ecf63fb77b9f208980
AmritSingh <amrit.s@ekoinfomatics.com>
2023-04-24 17:31:23 +0530

    script updated with venv

--- test_script.sh
+++ test_script.sh
@@ -1,2 +1,9 @@
 sudo su
-mkdir test_folder_new
+if [-d "venv"]
+then
+    echo "Venv already exist"
+else
+    pip install virtualenv
+    python3 -m virtualenv venv
+    echo "virtual environment created"
+# mkdir test_folder_new
